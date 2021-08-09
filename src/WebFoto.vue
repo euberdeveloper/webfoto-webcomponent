54a074bf3b3186ec47d9e066480d41b08b247790
euberdeveloper <euberdeveloper@gmail.com>
2021-08-09 22:13:37 +0200

    feat: add start code
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ src/WebFoto.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Ciao mona
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class WebFoto extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  margin: 0;
+}
+</style>
