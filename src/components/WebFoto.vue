34da74f42c907b0c5c1efa153190ecb1b90b2e69
euberdeveloper <euberdeveloper@gmail.com>
2021-08-11 23:34:16 +0200

    feat: add first prototype of date update
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/components/WebFoto.vue
+++ src/components/WebFoto.vue
@@ -2,18 +2,29 @@
   <div class="web-foto">
     <image-displayer class="image-displayer" :image="currentImagePath" :loading.sync="loadingImage" v-if="currentImagePath" />
     <loading-spinner class="loading-spinner" :color="spinnerColor" :show="loadingImage" />
-    <date-controller class="controllers" :value="currentImageDate" :dates="images" @current="goToLastImage" v-if="currentImageDate" />
+    <date-controller
+      class="controllers"
+      :value="currentImageDate"
+      :dates="images"
+      @increment="incrementImage($event)"
+      @decrement="decrementImage($event)"
+      @current="goToLastImage"
+      v-if="currentImageDate"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import * as dayjs from 'dayjs';
 
 import { getImages } from "@/utils/api";
 
 import ImageDisplayer from "@/components/gears/ImageDisplayer.vue";
 import LoadingSpinner from "@/components/gears/LoadingSpinner.vue";
 import DateController from "@/components/gears/Controllers.vue";
+
+type Extent = "year" | "month" | "day" | "hours" | "minutes";
 
 @Component({
   components: {
@@ -37,10 +48,14 @@ export default class WebFoto extends Vue {
   /* DATA */
 
   private images: Date[] = [];
-  private currentImageDate: Date | null = null;
+  private currentImageIndex: number | null = null;
   private loadingImage = false;
 
   /* GETTERS */
+
+  get currentImageDate(): Date | null {
+    return this.currentImageIndex ? this.images[this.currentImageIndex] : null;
+  }
 
   get currentImagePath(): string | null {
     return this.currentImageDate ? `${this.apiUrl}/${this.name}/${this.name}_${this.currentImageDate.toISOString()}.jpg` : null;
@@ -48,8 +63,52 @@ export default class WebFoto extends Vue {
 
   /* METHODS */
 
+  private extentToMillis(extent: Extent): number {
+    switch (extent) {
+      case "year":
+        return 31536000000;
+      case "month":
+        return 2592000000;
+      case "day":
+        return 86400000;
+      case "hours":
+        return 3600000;
+      case "minutes":
+        return 60000;
+    }
+  }
+
   goToLastImage(): void {
-    this.currentImageDate = this.images[this.images.length - 1];
+    this.currentImageIndex = this.images.length - 1;
+  }
+
+  incrementImage(extent: Extent): void {
+    if (this.currentImageIndex && this.currentImageDate) {
+      const targetDate = new Date(+this.currentImageDate + this.extentToMillis(extent));
+
+      let index = this.currentImageIndex;
+      while (this.images[index] < targetDate && index < this.images.length) {
+        index++;
+      }
+
+      if (this.images[index]) {
+        this.currentImageIndex = index;
+      }
+    }
+  }
+  decrementImage(extent: Extent): void {
+    if (this.currentImageIndex && this.currentImageDate) {
+      const targetDate = new Date(+this.currentImageDate - this.extentToMillis(extent));
+
+      let index = this.currentImageIndex;
+      while (this.images[index] > targetDate && index > 0) {
+        index--;
+      }
+
+      if (this.images[index]) {
+        this.currentImageIndex = index;
+      }
+    }
   }
 
   /* LIFE CYCLE */
