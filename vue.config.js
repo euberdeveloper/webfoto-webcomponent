a887838251102c55fe4a806bdbeda94e71824783
euberdeveloper <euberdeveloper@gmail.com>
2021-09-15 00:06:50 +0200

    build: fix obfuscator identifiers
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,8 @@ const WebpackObfuscator = require('webpack-obfuscator');
 module.exports = {
     chainWebpack: config => {
         config.plugin('obfuscate').use(new WebpackObfuscator({
-            identifierNamesGenerator: 'mangled-shuffled'
+            identifierNamesGenerator: 'mangled-shuffled',
+            identifiersPrefix: 'wf_'
         }));
     }
 };
